8aa7918f4826ada41693655ed8881ef39a48e3b8
Антон Домолазов <anton@domelaz.ru>
2015-07-27 12:28:39 +0300

    Параметризация дерективы tvm-rule

--- src/js/directives/rule.js
+++ src/js/directives/rule.js
@@ -1,5 +1,10 @@
 app.directive('tvmRule', function() {
+    var link = function(scope, element, attributes) {
+        scope.title = attributes['tvmTitle'];
+        scope.descr = attributes['tvmDescr'];
+    };
     return {
-        templateUrl: 'dir/rule.html'
+        templateUrl: 'dir/rule.html',
+        link : link
     };
 });
